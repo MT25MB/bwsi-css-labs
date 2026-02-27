a497ebcb70ccfaa4ecf227197a51c096a35fa4c3
Milan M. <148401452+MT25MB@users.noreply.github.com>
2026-02-27 01:42:00 -0500

    Added tests for lab 1c and 1d

---
+++ tests/tests_1d.py
@@ -0,0 +1,10 @@
+
+import pytest
+from labs.lab_1.lab_1d import two_sum
+
+def test_two_sum():
+    assert two_sum([2, 7, 11, 15], 9) == [0, 1]          # Test case
+    assert two_sum([3, 2, 4], 6) == [1, 2]               # Test for multiple pairs that add up to target
+    assert two_sum([3, 3], 6) == [0, 1]                  # Test for duplicate numbers that add up to target
+    assert two_sum([-1, -2, -3, -4, -5], -8) == [2, 4] # Test for negative numbers
+    assert two_sum([0, 4, 3, 0], 0) == [0, 3]          # Test for zeroes in the list
