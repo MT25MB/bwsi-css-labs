fb9de36418747e5bab4af13ec33e65648996223f
Milan M. <148401452+MT25MB@users.noreply.github.com>
2026-02-27 01:53:05 -0500

    Fix V3
    please work

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -1,8 +1,8 @@
 """
 lab_1b.py
 
-This is a script that implements a simple calculator. It takes two numbers and an operation,
-then performs the operation and returns the result. 
+This is a script that implements a simple calculator. It takes two numbers
+and an operation, then performs the operation and returns the result.
 
 The script asks the user to input the numbers and the operation to be performed,
 and prints the result to the terminal window.
@@ -11,8 +11,9 @@ and prints the result to the terminal window.
 
 def simple_calculator(operation: str, num1: float, num2: float) -> float:
     """
-    Function that takes in two numbers and an operation (add, subtract, multiply, divide),
-    then performs the operation on the two numbers and returns the result.
+    Function that takes in two numbers and an operation
+    (add, subtract, multiply, divide), then performs the operation on the two
+    numbers and returns the result.
 
     Args:
         operation (str): The operation to perform ("add", "subtract", "multiply", "divide").
@@ -39,7 +40,7 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
 
 def main():
     
-    print(f"===== Simple Calculator =====")
+    print("===== Simple Calculator =====")
 
     workingNumOne = False
     workingNumTwo = False
@@ -61,7 +62,9 @@ def main():
             print("Error: pls give valid number")
 
     while not workingoperation:
-        operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+        operation = input(
+            "Enter the operation (add, subtract, multiply, divide): "
+        ).strip().lower()
         if operation in ["add", "subtract", "multiply", "divide"]:
             workingoperation = True
         else:
@@ -71,7 +74,9 @@ def main():
 
     # Perform the calculation and display the result
     result = simple_calculator(operation, num1, num2)
-    print(f"The result of {operation}ing {num1} and {num2} is: {result}")
+    print(
+        f"The result of {operation}ing {num1} and {num2} is: {result}"
+    )
 
 
 if __name__ == "__main__":
